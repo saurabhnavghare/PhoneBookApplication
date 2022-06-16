3a5e201a82ebac683defa5653f7334e11caf812a
saurabhnavghare <saurabhrn16@gmail.com>
2022-06-16 06:41:08 +0530

    PHON-11: Create method in serviceImpl and insert data into  server

--- src/main/java/com/bikkadIt/PhoneBook/service/ContactServiceImpl.java
+++ src/main/java/com/bikkadIt/PhoneBook/service/ContactServiceImpl.java
@@ -1,15 +1,21 @@
 package com.bikkadIt.PhoneBook.service;
 
 import com.bikkadIt.PhoneBook.model.Contact;
+import com.bikkadIt.PhoneBook.repository.ContactRepository;
 
 public class ContactServiceImpl implements ContactServiceI {
 
-
+	private ContactRepository contactRepository;
 
 	
 	public boolean saveContact(Contact contact) {
-		
-		return false;
+		Contact save = contactRepository.save(contact);
+
+		if (save != null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
