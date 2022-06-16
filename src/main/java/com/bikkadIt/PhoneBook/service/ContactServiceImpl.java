cd2e92d3e7519de6143d9a475d0dd3a6f34046cc
saurabhnavghare <saurabhrn16@gmail.com>
2022-06-16 06:10:10 +0530

    PHON-9: write method declaration in contactServiceI

--- src/main/java/com/bikkadIt/PhoneBook/service/ContactServiceImpl.java
+++ src/main/java/com/bikkadIt/PhoneBook/service/ContactServiceImpl.java
@@ -1,5 +1,15 @@
 package com.bikkadIt.PhoneBook.service;
 
+import com.bikkadIt.PhoneBook.model.Contact;
+
 public class ContactServiceImpl implements ContactServiceI {
+
+
+
+	
+	public boolean saveContact(Contact contact) {
+		
+		return false;
+	}
 
 }
